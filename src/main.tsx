d4dee9900ef3c3edf9f7fb52b90e8dc666f0f9ff
Ricardo Anjos <ricardoanj.14@gmail.com>
2023-03-11 21:21:33 -0300

    exclui tema

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
-import { ThemeProvider } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-import { darkTheme } from './theme/DarkTheme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
       <App />
-    </ThemeProvider>
   </React.StrictMode>,
 )
